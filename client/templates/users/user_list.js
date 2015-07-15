2cdea1d7b5727f77bb7e67d40788ad5f7d61c95e
franckie20 <franck.verschuur@gmail.com>
2015-07-15 14:43:46 +0200

    User implementation
    User implementation

---
+++ client/templates/users/user_list.js
@@ -0,0 +1,18 @@
+var usersData = [
+ {
+  title: 'Introducing Telescope',
+  url: 'http://sachagreif.com/introducing-telescope/'
+ },
+ {
+  title: 'Meteor',
+  url: 'http://meteor.com'
+ },
+ {
+  title: 'The Meteor Book',
+  url: 'http://themeteorbook.com'
+ }
+];
+
+Template.usersList.helpers({
+users: usersData
+});
