a2b4ef10a4c7e7361de9ec4671e9e40054b6ae7a
franckie20 <franck.verschuur@gmail.com>
2015-07-16 16:06:44 +0200

    Quick fixes
    Quick fixes

--- client/templates/main/main.js
+++ client/templates/main/main.js
@@ -1,12 +1,17 @@
-    // Setup an event listener to make an API call once auth is complete
+
+	// Setup an event listener to make an API call once auth is complete
     onLinkedInLoad = function onLinkedInLoad() {
         IN.Event.on(IN, "auth", getProfileData);
     }
-	// Check if the user has been here before
-	function loggedInBefore() {
-	}
+	
     // Handle the successful return from the API call
     onSuccess = function onSuccess(data) {
+		
+		userFirstname = data.firstName;
+		document.getElementById("fname").innerHTML = "Firstname: " + userFirstname; 
+		userLastname = data.lastName;
+		document.getElementById("lname").innerHTML = "Lastname: " + userLastname; 
+		
 		var userWithSameId = LoggedUser.findOne({userid: data.id});
 		if(userWithSameId == null) {
 			Meteor.call('insertUserName', data.id, data.firstName, data.lastName);
@@ -14,6 +19,11 @@
 		console.log(userWithSameId);
         console.log(data);
     }
+	
+	getUserFirstname = function getUserFirstname() {
+		return userFirstname;
+	}
+	
     // Handle an error response from the API call
     function onError(error) {
         console.log(error);
---
+++ client/templates/profile/profile.js
@@ -0,0 +1,4 @@
+	/*
+	document.getElementById("fname").innerHTML = "Firstname: " + userFirstname;
+	document.getElementById("lname").innerHTML = "Lastname: " + userLastname;
+	*/
