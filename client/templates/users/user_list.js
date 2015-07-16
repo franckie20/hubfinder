0e28093faab21630e52432c8c5e9324fd1bb6a46
Pertulibaba <jasperdewaard@hotmail.com>
2015-07-16 12:42:25 +0200

    Quick fix
    Quick fix

--- client/templates/users/user_list.js
+++ client/templates/users/user_list.js
@@ -1,20 +1,5 @@
-var usersData = [
- {
-  firstname: 'Jasper',
-  lastname: 'de Waard',
-  url: 'http://sachagreif.com/introducing-telescope/'
- },
- {
-  firstname: 'Franck',
-  lastname: 'Verschuur',
-  url: 'http://meteor.com'
- },
- {
-  firstname: 'Alex',
-  lastname: 'Jongman',
-  url: 'http://themeteorbook.com'
- }
-];
 Template.usersList.helpers({
-users: usersData
+users: function() {
+return LoggedUser.find();
+}
 });
