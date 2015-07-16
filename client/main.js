c0e81011f2298bb44fe8aa3a1f29678ff037774f
Pertulibaba <jasperdewaard@hotmail.com>
2015-07-16 12:28:17 +0200

    User implementation working
    User implementation working

--- client/main.js
+++ client/main.js
@@ -2,33 +2,33 @@
     onLinkedInLoad = function onLinkedInLoad() {
         IN.Event.on(IN, "auth", getProfileData);
     }
-	
 	// Check if the user has been here before
 	function loggedInBefore() {
-	
 	}
-
     // Handle the successful return from the API call
-    function onSuccess(data) {
+    onSuccess = function onSuccess(data) {
+		var userWithSameId = LoggedUser.findOne({userid: data.id});
+		if(userWithSameId == null) {
+			Meteor.call('insertUserName', data.id, data.firstName, data.lastName);
+		}
+		console.log(userWithSameId);
         console.log(data);
-		var loggedUser = Meteor.LinkedInUsers.insert({ firstname: data.firstName });
-		console.log(data.firstName);
     }
-
     // Handle an error response from the API call
     function onError(error) {
         console.log(error);
     }
-
     // Use the API call wrapper to request the member's profile data
-    function getProfileData() {
-        IN.API.Raw("/people/~").result(onSuccess).error(onError);
+    getProfileData = function getProfileData() {
+		IN.API.Raw("/people/~").result(onSuccess).error(onError);
     }
-	
+
 	logoutUser = function logoutUser() {
 		IN.User.logout(logoutSucces);
 	}
-	
+
 	logoutSucces = function logoutSucces() {
 		console.log("Logout successful!");
 	}
+
+
