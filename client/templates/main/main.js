d8ab48277883522674765ff350862bfa3b6938de
franckie20 <franck.verschuur@gmail.com>
2015-07-20 14:28:55 +0200

    QuickFixes
    QuickFixes

--- client/templates/main/main.js
+++ client/templates/main/main.js
@@ -3,16 +3,16 @@
     onLinkedInLoad = function onLinkedInLoad() {
 		IN.Event.on(IN, "auth", getProfileData);
     }
-	
+
 	var userLoggedIn = false;
-	
+
     // Handle the successful return from the API call
     onSuccess = function onSuccess(data) {
-		
-		userFirstname = data.firstName; userLastname = data.lastName; userEmail = data.emailAddress; 
+
+		userFirstname = data.firstName; userLastname = data.lastName; userEmail = data.emailAddress;
 		userHeadline = data.headline; userPicture = data.pictureUrl; userConnections = data.numConnections;
 		userId = data.id; userSummary = data.summary;
-		
+
 		var userWithSameId = LoggedUser.findOne({userid: userId});
 		if(userWithSameId == null) {
 			Meteor.call('insertUserName', userId, userFirstname, userLastname, userEmail, userHeadline, userPicture, userConnections, userSummary);
@@ -21,29 +21,29 @@
 		else {
 			// Update the MongoDB information (If there's a LinkedIn profile update)
 			LoggedUser.update(
-				{_id: userWithSameId._id}, 
+				{_id: userWithSameId._id},
 				{$set: {firstname: userFirstname, lastname: userLastname, email: userEmail, headline: userHeadline, picture: userPicture, connections: userConnections, summary: userSummary}}
 			);
-			
+
 			onLoginSuccess(data);
 
 			console.log(userWithSameId);
 			console.log(data);
 		}
     }
-	
+
 	function onLoginSuccess(data) {
 		// Set the login state to true
 		Meteor.call('setLoginStateTrue', function(error, result) {
 			 Session.set('setLoginStateResult', result);
 		});
-		
+
 		// Set the User profile object
 		Meteor.call('setProfileUser', data.id, function(error, result) {
 			 Session.set('setProfileUserResult', result);
 		});
 	}
-	
+
     // Handle an error response from the API call
     function onError(error) {
         console.log(error);
@@ -65,5 +65,3 @@
 		});
 		console.log("Logout successful!");
 	}
-
-
